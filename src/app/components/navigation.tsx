19d30599a41237c3ff9923247fb8964d771396f8
Anders Norrback Bornholm <anders@bornholm.se>
2024-10-28 22:17:17 +0100

    Added bio and hamburger nav for small screens

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from 'react'
+import { Heading } from './heading'
+import Hamburger from 'hamburger-react'
+
+export const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <>
+      <div className='lg:pt-4 flex-row hidden md:flex'>
+        <a href='/#music'>
+          <Heading text='Music' style='h3' />
+        </a>
+        <span className='px-3'>|</span>
+        <a href='/#streaming-services'>
+          <Heading text='Streaming Services' style='h3' />
+        </a>
+        <span className='px-3'>|</span>
+        <a href='/#contact'>
+          <Heading text='Contact' style='h3' />
+        </a>
+        <span className='px-3'>|</span>
+        <a href='/bio'>
+          <Heading text='Bio' style='h3' />
+        </a>
+      </div>
+      <div className='absolute top-0 right-0 flex flex-col items-end'>
+        <div className='mt-4 mr-4 z-20 md:hidden'>
+          <Hamburger onToggle={() => setMenuOpen(!menuOpen)} />
+        </div>
+        {menuOpen && (
+          <div className='text-6xl absolute z-10 top-0 w-[300px] pt-12 flex flex-col space-y-2 rounded-lg px-5 py-4 m-3 border-4 border-[#F515AC] outline outline-4 outline-[#5EC4FF] bg-black bg-opacity-60 font-mono'>
+            <a href='/#music'>
+              <Heading text='Music' style='h3' />
+            </a>
+            <a href='/#streaming-services'>
+              <Heading text='Streaming Services' style='h3' />
+            </a>
+            <a href='/#contact'>
+              <Heading text='Contact' style='h3' />
+            </a>
+            <a href='/bio'>
+              <Heading text='Bio' style='h3' />
+            </a>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Heading } from './components/heading'
+import { Navigation } from './components/navigation'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -31,41 +31,7 @@ export default function RootLayout({
           <source src='background2.mp4' type='video/mp4' />
         </video>
         <main className='flex min-h-screen flex-col items-center'>
-          <div className='lg:pt-4 flex-row hidden md:flex'>
-            <a href='/#music'>
-              <Heading text='Music' style='h3' />
-            </a>
-            <span className='px-3'>|</span>
-            <a href='/#streaming-services'>
-              <Heading text='Streaming Services' style='h3' />
-            </a>
-            <span className='px-3'>|</span>
-            <a href='/#contact'>
-              <Heading text='Contact' style='h3' />
-            </a>
-            <span className='px-3'>|</span>
-            <a href='/bio'>
-              <Heading text='Bio' style='h3' />
-            </a>
-          </div>
-          <div className='absolute top-0 right-0 flex flex-col border'>
-            <div>=</div>
-            <div className='z-10 flex flex-col space-y-2 xl:hidden rounded-lg px-5 py-4 m-3 border-4 border-[#F515AC] outline outline-4 outline-[#5EC4FF] bg-black bg-opacity-60 font-mono'>
-              <div>=</div>
-              <a href='/#music'>
-                <Heading text='Music' style='h3' />
-              </a>
-              <a href='/#streaming-services'>
-                <Heading text='Streaming Services' style='h3' />
-              </a>
-              <a href='/#contact'>
-                <Heading text='Contact' style='h3' />
-              </a>
-              <a href='/bio'>
-                <Heading text='Bio' style='h3' />
-              </a>
-            </div>
-          </div>
+          <Navigation />
           {children}
         </main>
       </body>
