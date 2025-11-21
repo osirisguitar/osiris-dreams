3199f514bcedfa8789d3c6d1e14c5309857c1d8b
Anders Norrback Bornholm <anders@bornholm.se>
2025-11-21 10:48:59 +0100

    Added permanent redirect for The Cybergenix Conspiracy album to its first song (albums are no longer handled as albums)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/songs/the-cybergenix-conspiracy',
+        destination: '/songs/biomechanoid-prototype',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
